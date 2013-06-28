b9f04d2dcb1c14a068824e4f0029aa2ebf627260
Dmitry Monakhov <dmonakhov@openvz.org>
2013-06-28 18:33:15 +0400

    Add test module regression - generic regression test suite
    Accept two directories with keyval files (usualy result directories for a test)
    and compare given fields according to given pattern.
    
    EXAMPLE: ext4 patch queue validation scenario
    
     Following example run two static analyzers('smatch' and 'sparse') on vanila
     kernel.
     Step 1: Each analizer produces keyvalue file with number of warnings,errors
     Step 2: Apply patches against vanila kernel and run analizers one more time.
     Step 3: Compare number of warnings and errors collected from vanilla and
             patched kernel and fail if number of errors increased.
    
    I know that we already has some functionality in TKO, but it is too complex
    for some usecases. And if we have regression helper which works as simle test
    this will perfectly cover patch validation scenario for most develpers.
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>

---
+++ regression/regression.py
@@ -0,0 +1,50 @@
+import logging
+from autotest.client import test, utils
+from autotest.client.shared import error
+
+class regression(test.test):
+    version = 1
+
+    def execute(self, old, new, compare_list):
+        """
+        Compare two (old, new) keyvalue files
+
+        compare_list: list of tuples ('field_name', 'regression_op')
+        regression_op(a,b) return 0 if not regression, and ~0 otherwise
+        """
+        kv_old = utils.read_keyval(old)
+        kv_new = utils.read_keyval(new)
+        failed = 0;
+        first_regression = None;
+        logging.info('========= Comparison table for %30s '
+                     '=========' %  self.tagged_testname)
+        logging.info("%20s | %10s | %10s | %10s | %10s | %s" %
+                         ('field name', 'old value', 'new value',
+                          'cmp res', 'status', 'cmp function'))
+        for field, cmpfn in compare_list:
+            if not field in kv_old:
+                raise error.TestError('Cant not find field:%s in %s' \
+                                          % (field, old + '/keyval'))
+            if not field in kv_new:
+                raise error.TestError('Cant not find field:%s in %s' \
+                                          % (field, new + '/keyval'))
+            res = cmpfn(kv_old[field], kv_new[field])
+            if res:
+                failed += 1
+                msg = 'FAIL'
+                if not first_regression:
+                    first_regression = field
+            else:
+                msg = 'OK'
+            logging.info("%20s | %10s | %10s | %5s | %5s | %s" %
+                         (field, kv_old[field],
+                          kv_new[field],res, msg, cmpfn))
+
+        logging.info("========= RESULT: total:%10d failed:%10d "
+                     "==================" %
+                     (len(compare_list), failed))
+        if failed:
+            raise error.TestError('Regression found at field:%s, old:%s, new:%s ' %
+                                  (first_regression,
+                                   kv_old[first_regression],
+                                   kv_new[first_regression]))
