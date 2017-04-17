b9c7fd8ae7de893de72c6dd6c1daf628e3dc9b26
Ramya BS <ramya@linux.vnet.ibm.com>
2017-04-17 16:19:45 +0530

    Fix for avahi_autoipd ,crontab ,pax path issues and import issue.
    crontab/pax/avahi_autoipd : updated the test path in control file ,and also tests failed as import of the (shutil, from autotest.client import utils etc)modules were missing
    so added the same in .py files.
    
    Signed-off-by:ramyabs<ramya@linux.vnet.ibm.com>

--- linux-tools/pax/pax.py
+++ linux-tools/pax/pax.py
@@ -1,7 +1,10 @@
 #!/bin/python
 import os
+import shutil
 import logging
 
+from autotest.client.shared  import software_manager
+from autotest.client import utils
 from autotest.client import test
 from autotest.client.shared import error
 
